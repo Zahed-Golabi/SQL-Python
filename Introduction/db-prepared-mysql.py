5965fa024db588d2de0870b9614fd5710675e3e7
Zahed-Golabi <golabizahed@gmail.com>
2023-04-18 00:33:48 +0430

    db-prepared-mysql added

---
+++ Introduction/db-prepared-mysql.py
@@ -0,0 +1,17 @@
+import mysql.connector as mysql
+
+
+def main():
+    db = mysql.connect(host="127.0.0.1", user='zahed', password='94723115')
+    cur = db.cursor()
+
+    cur.execute("SELECT VERSION()")
+    version = cur.fetchone()[0]
+    print(f"MySQL version: {version}")
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
