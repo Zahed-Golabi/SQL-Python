e4f0c4f29cc41b814dcd259e466ad0c438fe80ec
Zahed Golabi <golabizahed@gmail.com>
2023-05-06 18:32:23 +0430

    do_menu function

--- OOP-SQL/domain.py
+++ OOP-SQL/domain.py
@@ -16,6 +16,31 @@ def connect():
     GLOBASLS["db"] = db
     return db
 
+def do_menu():
+    while True:
+        menue = [
+            "A) Add domain",
+            "F) Find domain",
+            "E) Edit domain",
+            "L) List domains",
+            "D) Delete domain",
+            "X) Drop table and exit",
+            "Q) Quit"
+        ]I
+        print()
+        for option in menue:
+            print(option)
+        response = input("Select an action or Q to quit").upper()
+        if len(response) != 1:
+            print("\nInput too long or empty")
+            continue
+        elif response in "AFELDXQ":
+            break
+        else:
+            print("\nInvalid response")
+            continue
+    return response
+
 
 
 
